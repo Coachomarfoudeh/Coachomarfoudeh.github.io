9fb403430ad1d26196d71b69e8906f1dc7df47b8
Coachomarfoudeh <155221571+Coachomarfoudeh@users.noreply.github.com>
2023-12-30 15:49:41 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,38 @@
-$(document).ready(function () {
-  let exercisesData; // Store exercises data globally
+let exercisesData; // Store exercises data globally
 
+// Define the searchExercise function before using it
+function searchExercise(input) {
+  const exerciseDetails = $("#exerciseDetails");
+  exerciseDetails.html("");
+
+  const filteredExercises = exercisesData.filter((exercise) =>
+    exercise.name.toLowerCase() === input.toLowerCase()
+  );
+
+  if (filteredExercises.length > 0) {
+    filteredExercises.forEach((exercise) => {
+      const exerciseDiv = $("<div>").addClass("exercise");
+
+      $("<h2>").text(exercise.name).appendTo(exerciseDiv);
+      $("<p>").text(exercise.description).appendTo(exerciseDiv);
+
+      const exerciseVideo = $("<iframe>").attr({
+        src: exercise.video,
+        width: "560",
+        height: "315",
+        title: "Exercise Video",
+        allowfullscreen: true,
+      });
+      exerciseVideo.appendTo(exerciseDiv);
+
+      exerciseDiv.appendTo(exerciseDetails);
+    });
+  } else {
+    exerciseDetails.html("<p>No exercises found.</p>");
+  }
+}
+
+$(document).ready(function () {
   // Load exercises data from JSON file
   $.getJSON("exercises.json", function (data) {
     exercisesData = data.exercises;
@@ -20,37 +52,6 @@ $(document).ready(function () {
       }
     });
   });
-
-  function searchExercise(input) {
-    const exerciseDetails = $("#exerciseDetails");
-    exerciseDetails.html("");
-
-    const filteredExercises = exercisesData.filter((exercise) =>
-      exercise.name.toLowerCase() === input.toLowerCase()
-    );
-
-    if (filteredExercises.length > 0) {
-      filteredExercises.forEach((exercise) => {
-        const exerciseDiv = $("<div>").addClass("exercise");
-
-        $("<h2>").text(exercise.name).appendTo(exerciseDiv);
-        $("<p>").text(exercise.description).appendTo(exerciseDiv);
-
-        const exerciseVideo = $("<iframe>").attr({
-          src: exercise.video,
-          width: "560",
-          height: "315",
-          title: "Exercise Video",
-          allowfullscreen: true,
-        });
-        exerciseVideo.appendTo(exerciseDiv);
-
-        exerciseDiv.appendTo(exerciseDetails);
-      });
-    } else {
-      exerciseDetails.html("<p>No exercises found.</p>");
-    }
-  }
 
   // Event listener for the search button
   $("button").on("click", function () {
